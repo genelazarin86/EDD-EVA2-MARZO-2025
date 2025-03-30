2f01731137e641e0b87652be66dcaa105af52b9d
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2025-03-30 13:20:30 -0600

    Add files via upload

---
+++ EVA2_4_DOUBLE_LINKED_LIST/src/main/java/com/mycompany/eva2_4_double_linked_list/EVA2_4_DOUBLE_LINKED_LIST.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_4_double_linked_list;
+
+/**
+ *
+ * @author Admin
+ */
+public class EVA2_4_DOUBLE_LINKED_LIST {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ EVA2_4_DOUBLE_LINKED_LIST/src/main/java/com/mycompany/eva2_4_double_linked_list/Nodo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eva2_4_double_linked_list;
+
+/**
+ *
+ * @author Admin
+ */
+public class Nodo {
+    
+}
